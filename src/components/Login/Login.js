909e9ef59f13e407c9fe4049d69225c3444cb364
noahnim <nmedoff123@gmail.com>
2022-06-05 19:50:12 -0700

    made and styled login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,5 +1,23 @@
-import "./Login.css";
+import styles from "./Login.module.css";
+import { Link } from "react-router-dom";
 
-const Login = (props) => {};
+const Login = (props) => {
+  return (
+    <div className={`${styles["login-container"]}`}>
+      <div className={`${styles["login-form"]}`}>
+        {props.user.map((user) => (
+          <div>
+            <h2>Face Scan Complete.....</h2>
+            <h3>User Verified</h3>
+            <h4>Welcome {user.name}</h4>
+            <Link to="/welcome-page">
+              <button className={`${styles["login-button"]}`}>Login</button>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Login;
