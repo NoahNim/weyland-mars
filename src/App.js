cfcec6fdcc859962585a80b3570e614e5f2d5881
noahnim <nmedoff123@gmail.com>
2022-06-05 20:05:13 -0700

    link to welcome page and made base welcome component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import { users, files, important, messages, logs } from "./Data";
 import { Routes, Route } from "react-router-dom";
 import Login from "./components/Login/Login";
+import Welcome from "./components/WelcomePage/Welcome";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Login user={users} />} />
+        <Route path="welcome-page" element={<Welcome newsfeed={important} />} />
       </Routes>
     </div>
   );
---
+++ src/components/WelcomePage/Welcome.js
@@ -0,0 +1,5 @@
+import styles from "./Welcome.module.css";
+
+const Welcome = (props) => {};
+
+export default Welcome;
